165b72cc2037172084f1c93e0bdf9455f04a05d2
nimaaskarian <nimaaskarian.work@outlook.com>
2025-08-29 20:50:10 +0330

    fix: Fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 dir="$(diraname "$0")"
 name=Sinister_2012_720p_BrRip_RARBG
-sudo cp "$dir/mkvtoolnix.download.list" /etc/apt/sources.list.d/
+sudo cp "$dir/mkv" /etc/apt/sources.list.d/mkvtoolnix.download.list
 sudo apt update
 sudo apt install -y mkvtoolnix
 
